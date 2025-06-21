a372719b3f5641d186674d602272c18096721e2a
bayaraa2552 <bayaraa1656@gmail.com>
2025-06-21 18:45:43 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,205 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-vvd",
+      "name": "vvd",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.530401867656775
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.14348426212991683,
+          "pitch": 0.6523213437326056,
+          "rotation": 3.141592653589793,
+          "target": "1-coridor"
+        },
+        {
+          "yaw": 0.2931298848764019,
+          "pitch": 0.15795665483120302,
+          "rotation": 7.0685834705770345,
+          "target": "2-untlagiin-uruu"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-coridor",
+      "name": "coridor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.530401867656775
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4564726217605788,
+          "pitch": -0.0004933562060216445,
+          "rotation": 10.995574287564278,
+          "target": "2-untlagiin-uruu"
+        },
+        {
+          "yaw": 0.4374127368122984,
+          "pitch": -0.0005010874014885758,
+          "rotation": 7.853981633974483,
+          "target": "3-ugaalgiin-uruu"
+        },
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "4-tom-uruu-galtogoo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-untlagiin-uruu",
+      "name": "untlagiin uruu",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -2.935105509204263,
+        "pitch": 0.17242641629084154,
+        "fov": 1.530401867656775
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6155605328459117,
+          "pitch": 0.06968177646337814,
+          "rotation": 0,
+          "target": "1-coridor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-ugaalgiin-uruu",
+      "name": "ugaalgiin uruu",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -2.472993304377338,
+        "pitch": -0.16140070051896238,
+        "fov": 1.530401867656775
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3971746398634366,
+          "pitch": 0.09754233331631745,
+          "rotation": 0,
+          "target": "1-coridor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-tom-uruu-galtogoo",
+      "name": "tom uruu galtogoo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.2996581438482853,
+        "pitch": -0.03883475228755273,
+        "fov": 1.530401867656775
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0970479648969196,
+          "pitch": 0.11303197764885908,
+          "rotation": 1.5707963267948966,
+          "target": "2-untlagiin-uruu"
+        },
+        {
+          "yaw": -1.4724372367271457,
+          "pitch": 0.1115312229146177,
+          "rotation": 4.71238898038469,
+          "target": "2-untlagiin-uruu"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
